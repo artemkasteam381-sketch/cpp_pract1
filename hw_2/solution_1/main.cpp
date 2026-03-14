732ff4531d7e051bc55e2823b5b17941b73bf1ec
artemkasteam381-sketch <artemka.steam.381@gmail.com>
2026-03-14 15:09:16 +0300

    Create main.cpp

---
+++ hw_2/solution_1/main.cpp
@@ -0,0 +1,8 @@
+#include "config.hpp"
+#include <iostream>
+int main() {
+  const int Config::kMax;
+  std::cout << Config::kMax << "\n"; // (1)
+  const int* p = &Config::kMax; // (2) uncomment - перед Config стоит ссылка
+  std::cout << *p << "\n";
+}

8abcc5c48e31a3ce9a7ef813530a7a06ed91e981
artemkasteam381-sketch <artemka.steam.381@gmail.com>
2026-03-14 15:10:26 +0300

    Update main.cpp

--- hw_2/solution_1/main.cpp
+++ hw_2/solution_1/main.cpp
@@ -1,7 +1,7 @@
 #include "config.hpp"
 #include <iostream>
 int main() {
-  const int Config::kMax;
+  const int Config::kMax; // теперь память выделена внутри функции и мы можем обратиться по ссылке
   std::cout << Config::kMax << "\n"; // (1)
   const int* p = &Config::kMax; // (2) uncomment - перед Config стоит ссылка
   std::cout << *p << "\n";
